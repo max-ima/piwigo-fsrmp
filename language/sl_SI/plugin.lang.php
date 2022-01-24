1d073a59014c8839481d06b5d66ac36a606ec2f3
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-24 18:22:09 +0100

    sl_SI
    Slovenian language

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+$lang['Fsrmp'] = 'Fsrmp filter';
+
+
+// batch manager
+$lang['Modified since less than %d %s'] = 'Spremenjeno od manj kot %d %s';
+$lang['mmin'] = 'minut';
+$lang['mtime'] = 'dni';
+$lang['Modified since previous bm.metadata (%d files), since less than about %d %s'] = '';
+
+
+// home
+$lang['What Fsrmp can do for me?'] = 'Kaj lahko Fsrmp naredi zame?';
+
+
+// config
+$lang['Check the predefined filters you want to enable.'] = 'Preverite vnaprej določene filtre, ki jih želite omogočiti.';
+$lang['Here you can set two filters to grab pictures list on your server, using Linux <em>find</em> command:'] = 'Tukaj lahko nastavite dva filtra za zajemanje seznama slik na vašem strežniku z ukazom <em>find</em> v Linuxu: ';
+$lang['to grab files modified within lastest 60 minutes;'] = 'zgrabiti datoteke, spremenjene v zadnjih 60 minutah;';
+$lang['to grab files modified within lastest 7 days;'] = 'zgrabiti datoteke, spremenjene v zadnjih 7 dneh;';
+$lang['to follow symlinks;'] = 'slediti simbolnim povezavam;';
+$lang['to list files;'] = 'seznam datotek;';
+$lang['check your man page.'] = 'preverite svojo glavno stran.';
+$lang['Since'] = 'Od';
+$lang['Number'] = 'Številka';
+$lang['Unit'] = 'Enota';
+$lang['First filter'] = 'Prvi filter';
+$lang['Second filter'] = 'Drugi filter';
+$lang['Third filter'] = 'Tretji filter';
+$lang['Since the latest batch manager metadata update.'] = 'Od zadnje posodobitve metapodatkov upravitelja paketov.' ;
+$lang['Since about'] = 'Od približno';
+$lang['with'] = 'z';
+$lang['pictures'] = 'slikami';
+$lang['or since about'] = 'ali od približno';
+$lang['Due to ceiling of time interval, some updated pictures may remain in the filter.'] = 'Zaradi zgornje meje časovnega intervala lahko nekatere posodobljene slike ostanejo v filtru.';
+?>
